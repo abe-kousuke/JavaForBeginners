e144c8025ced2afcd8bd96fad6a71e453c610b4c
zeal24360056@fsgcl.jp <zeal24360056@fsgcl.jp>
2025-05-26 10:17:32 +0900

    四角形に色を追加

--- src/RectTest.java
+++ src/RectTest.java
@@ -1,6 +1,25 @@
 
 public class RectTest extends MyFrame {
 	public void run() {
-		fillRect(30,50,200,100);
+		setColor(255,255,0);
+		fillRect(30,40,50,100);
+		
+		setColor(255,0,255);
+		fillRect(130,40,50,100);
+		
+		setColor(188,266,232);
+		fillRect(30,200,150,10);
+		
+		setColor(0,0,0);
+		fillRect(90,160,30,30);
+		
+		setColor(125,125,125);
+		fillRect(100,250,100,100);
+		
+		setColor(255,255,255);
+		fillRect(125,280,50,50);
+		
+		
+		
 	}
 }

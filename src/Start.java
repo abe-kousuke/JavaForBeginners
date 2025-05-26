8968ca0eb0878bd04b840d46a367a1064179b0fc
zeal24360056@fsgcl.jp <zeal24360056@fsgcl.jp>
2025-05-26 10:50:25 +0900

    OvalTestの追加

--- src/Start.java
+++ src/Start.java
@@ -3,8 +3,8 @@ public class Start {
 
 	public static void main(String[] args) {
 		// TODO 自動生成されたメソッド・スタブ
-        System.out.println("あいうえお");
-        MyFrame frame1 = new RectTest();
+        System.out.println("プログラムを実行します");
+        MyFrame frame1 = new OvalTest();
 	}
 
 }

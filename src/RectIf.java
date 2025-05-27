bef13344c9ec05a869548b04cb6a281d17a32518
zeal24360056@fsgcl.jp <zeal24360056@fsgcl.jp>
2025-05-27 10:44:22 +0900

    Ex問題

--- src/RectIf.java
+++ src/RectIf.java
@@ -4,7 +4,7 @@ public class RectIf extends MyFrame{
 		int x = 30;
 		fillRect(x,80,10,100);
 		int i;
-		for(i = 0; i < 10; i++) {
+		for(i = 0; i < 20; i++) {
 			if(i==0) {
 				fillRect(x,80,10,100);
 				setColor(0,0,0);
@@ -39,13 +39,57 @@ public class RectIf extends MyFrame{
 			  }
 			    else if(i==8) {
 			    fillRect(x,80,10,100);
-			    setColor(130,130,130);
+			    setColor(110,110,110);
 			  }
-			    else {
+			    else if(i==9) {
+				 fillRect(x,80,10,100);
+				 setColor(120,120,120);
+			 }
+			    else if(i==11) {
+				fillRect(x,80,10,100);
+				setColor(105,105,105);
+			  }
+				 else if(i==12) {
 			    fillRect(x,80,10,100);
-			    setColor(0,128,0);
+			    setColor(90,90,90);
 			  }
-			x += 20;
+				 else if(i==13) {
+				 fillRect(x,80,10,100);
+				 setColor(75,75,75);
+			  }
+				 else if(i==14) {
+				 fillRect(x,80,10,100);
+				 setColor(45,45,45);
+			  }
+				 else if(i==15) {
+				 fillRect(x,80,10,100);
+				 setColor(30,30,30);
+			  }
+				 else if(i==16) {
+				 fillRect(x,80,10,100);
+				 setColor(15,15,15);
+			  }
+				 else if(i==17) {
+				 fillRect(x,80,10,100);
+				 setColor(10,10,10);
+			  }
+				 else if(i==18) {
+				 fillRect(x,80,10,100);
+				 setColor(7,7,7);
+		      }
+				 else if(i==19) {
+				 fillRect(x,80,10,100);
+				 setColor(5,5,5);
+			  }
+				 else if(i==20) {
+				  fillRect(x,80,10,100);
+				  setColor(0,0,0);
+			  }
+			     else {
+			     fillRect(x,80,10,100);
+			     setColor(128,128,128);
+			  }
+			x += 15;
 		}
 	}
 

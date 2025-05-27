f814947c185e16ec11c531911f802d2e82442103
zeal24360056@fsgcl.jp <zeal24360056@fsgcl.jp>
2025-05-27 09:57:41 +0900

    練習問題4-1

--- src/RectIf.java
+++ src/RectIf.java
@@ -5,11 +5,31 @@ public class RectIf extends MyFrame{
 		setColor(0,128,0);
 		int i;
 		for(i = 0; i < 9; i++) {
-			if(i==4) {
+			if(i==1) {
+				fillRect(x,90,10,100);
+			  }	else if(i==2) {
 				fillRect(x,100,10,100);
-			  }	else if(i==7) {
-				fillRect(x,60,10,100);
-			  } else {
+			  }
+			    else if(i==3) {
+				fillRect(x,110,10,100);
+			  }
+			    else if(i==4) {
+			    fillRect(x,120,10,100);
+			  }
+			    else if(i==5) {
+			    fillRect(x,130,10,100);
+			  }
+			    else if(i==6) {
+			    fillRect(x,140,10,100);
+			  }
+			    else if(i==7) {
+			    fillRect(x,150,10,100);
+			  }
+			    else if(i==8) {
+			    fillRect(x,160,10,100);
+			    }
+			  
+			  else {
 				fillRect(x,80,10,100);
 			}
 			x += 20;
